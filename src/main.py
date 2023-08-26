e6b791d6113d84b16910cf9865a91a1bd72de467
Ben Chung <psybc9@nottingham.ac.uk>
2023-08-26 14:08:27 +0100

    Add get ip route

--- src/main.py
+++ src/main.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='static')
+
+@app.route("/ip")
+def get_ip():
+    return request.remote_addr + '\n'
 
 @app.route("/")
 def main():
