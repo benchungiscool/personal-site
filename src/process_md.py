7d64f102ad5849ddfa1fa2c309c88f2c0d7ee657
Ben Chung <ben@benchung.dev>
2024-02-11 23:47:10 +0000

    I'd prefer to do tagging manually

--- src/process_md.py
+++ src/process_md.py
@@ -10,31 +10,9 @@ directory = targetdir
 isMarkdownFile = lambda filename: filename[-3:] == '.md'
 getFilePath = lambda filename: str(targetdir + "/" + filename)
 
-# Lambda functions related to tagging markdown files
-isTagged = lambda filename: filename.split("_")[0].isnumeric()
-scrapeTags = lambda markdownfiles: [int(filename.split("_")[0]) for filename in filter(isTagged, markdownfiles)]
-
-# Get tags
-def gettags():
-    if markdownindir():
-        return scrapeTags(markdownindir())
-    return [0]
-
-# Get all the markdown files in a directory
-def markdownindir():
-    return [filename for filename in listdir(targetdir) if isMarkdownFile(filename)]
-
-def addtags():
-    to_tag = filter(lambda filename: not isTagged(filename), markdownindir())
-    max_tag = max(gettags())
-    for filename in to_tag:
-        max_tag += 1
-        os.rename(getFilePath(filename), getFilePath(str(max_tag) + "_" + filename))
-
 # Get the content of all the markdown files in our target directory, rendered into html
 def getallmd():
     ret = ""
-    addtags()
     for filename in markdownindir():
         with open(getFilePath(filename), "r") as fc:
             contents = fc.read()
